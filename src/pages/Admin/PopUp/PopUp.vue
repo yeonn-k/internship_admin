6db1d9425a1c4cbad6b12c16df15ccaf6b611cc8
Yeon <yeon@Yeonui-MacBookPro.local>
2023-07-10 11:28:41 +0900

    WIP

---
+++ src/pages/Admin/PopUp/PopUp.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="backgroundBlack">
+    <div class="backgroundWhite"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PopUp",
+};
+</script>
+
+<style>
+.backgroundBlack {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(256, 256, 256, 0.4);
+}
+</style>
