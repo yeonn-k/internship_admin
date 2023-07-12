9c0eac6928e2006e7c473fce85b2c083c79ba5a2
AGNESBAEK <jw.agnes.baek@gmail.com>
2023-07-12 10:15:42 +0900

    Add: Admin 페이지 및 Card 컴포넌트 추가

--- src/pages/Admin/Admin.vue
+++ src/pages/Admin/Admin.vue
@@ -1,10 +1,70 @@
-<template><div>Admin</div></template>
+<template>
+  <div class="boardContainer">
+    <SummaryBoard class="summaryBoard" />
+    <AdminBoard
+      class="board"
+      title="Backlog"
+      :dataArray="filteredDatas('Backlog')"
+    />
+    <AdminBoard
+      class="board"
+      title="Progress"
+      :dataArray="filteredDatas('Progress')"
+    />
+    <AdminBoard class="board" title="Done" :dataArray="filteredDatas('Done')" />
+  </div>
+</template>
 
 <script>
+import AdminBoard from "./components/AdminBoard.vue";
+import SummaryBoard from "./components/SummaryBoard.vue";
+import data from "../../assets/contact.json";
+const contactDatas = data;
+
 export default {
   name: "AdminVue",
-  components: {},
+  components: {
+    AdminBoard,
+    SummaryBoard,
+  },
+  data() {
+    return {
+      contactDatas,
+    };
+  },
+  methods: {
+    filteredDatas(title) {
+      if (title === "Backlog") {
+        return this.contactDatas.filter((data) => data.status === "회신 대기");
+      } else if (title === "Done") {
+        return this.contactDatas.filter((data) => data.status === "회신 완료");
+      } else if (title === "Progress") {
+        return this.contactDatas.filter(
+          (data) => data.status === "회신중" || data.status === "추가 회신"
+        );
+      } else {
+        return [];
+      }
+    },
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss">
+@import "../../assets/scss/variables.scss";
+
+.boardContainer {
+  display: flex;
+  justify-content: space-between;
+  margin: 10px auto;
+  width: 90%;
+
+  .board {
+    width: 25%;
+  }
+
+  .summaryBoard {
+    width: 20%;
+  }
+}
+</style>
---
+++ src/pages/Admin/components/SummaryBoard.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="summaryBoard">
+    <h1 class="boardTitle">Summary</h1>
+    <div class="totalSummary">
+      <div>총 회신:</div>
+      <div>
+        <span class="number">8</span>
+        건
+      </div>
+    </div>
+    <div class="dataContainer">
+      <SummaryData type="status" />
+      <SummaryData type="department" />
+      <SummaryData type="type" />
+    </div>
+  </div>
+</template>
+
+<script>
+import SummaryData from "./SummaryData.vue";
+
+export default {
+  name: "SummaryBoard",
+  components: {
+    SummaryData,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "../../../assets/scss/variables.scss";
+
+.summaryBoard {
+  height: 100%;
+  background-color: $lightGrey;
+  border-radius: 5px;
+  font-family: $fontFamily;
+
+  .boardTitle {
+    display: flex;
+    justify-content: center;
+    margin: 20px 0;
+    font-size: 1.5rem;
+    font-weight: 900;
+  }
+
+  .totalSummary {
+    background-color: #ffffff;
+    width: 80%;
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 15px;
+    height: 20px;
+    border-radius: 5px;
+    font-weight: 900;
+    height: 40px;
+
+    .number {
+      font-weight: 900;
+      color: $primaryColor;
+    }
+  }
+}
+</style>
